bc8d33602b63acda2fd1730993a11345ac2646a5
Shakiru <Olaiya.adeolu@gmail.com>
2025-06-14 05:40:30 +0100

    calculator assignment submission

---
+++ app.js
@@ -0,0 +1,34 @@
+const btnEl = document.querySelectorAll("button")
+const inputFieldEl = document.querySelector("#result")
+
+for(let i = 0; i < btnEl.length; i++)
+{
+    btnEl[i].addEventListener("click", ()=>{
+        const btnValue = btnEl[i].textContent
+        if(btnValue === "C"){
+            clearResult()
+        }
+        else if(btnValue === "="){
+            calculateResult()
+            
+        }
+        else{
+            appendValue(btnValue)
+        }
+        
+    })
+}
+
+function clearResult()
+{
+    inputFieldEl.value = ""
+}
+
+function calculateResult()
+{
+    inputFieldEl.value = eval(inputFieldEl.value)
+}
+
+function appendValue(buttonValue){
+    inputFieldEl.value += buttonValue
+}
